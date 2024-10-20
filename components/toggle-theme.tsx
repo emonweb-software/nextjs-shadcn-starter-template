6a914f8599ea120907ea2cc93c5b8fa51dbcaedc
HOÀNG CÔNG MINH <101419067+HoangCongMin@users.noreply.github.com>
2024-10-20 21:13:32 +0700

    Fix comment/feature/dark light (#2)
    * build feature dark light
    
    * build feature dark light
    
    * fix comment
    
    * fix satus
    
    ---------
    
    Co-authored-by: Vuong Do <xoaicc.work@gmail.com>

--- components/toggle-theme.tsx
+++ components/toggle-theme.tsx
@@ -5,17 +5,51 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme ,theme} = useTheme();
+  const { setTheme } = useTheme();
+
+  const handleSunsetMode = () => {
+    const hour = new Date().getHours();
+    if (hour >= 6 && hour < 18) {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
   return (
-    <>
-      <Button onClick={() => setTheme(theme === "light" ? "dark" :"light")} variant="outline" size="icon">
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
-      </Button>
-      
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleSunsetMode()}>
+        Auto Dark Mode
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
