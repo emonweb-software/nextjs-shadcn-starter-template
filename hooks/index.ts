c4b47b1648f555ebfa96d04c636472d4dd72e337
minhkonzz <minhphm37@gmail.com>
2024-10-23 14:25:36 +0700

    build: plug react-hook-form with zod resolver and more form components

---
+++ hooks/index.ts
@@ -0,0 +1,2 @@
+export { default as useZodForm } from './use-zod-form';
+// export { default as useLocalStorage } from './use-local-storage'
---
+++ hooks/use-zod-form.ts
@@ -0,0 +1,30 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import {
+  useForm,
+  UseFormProps,
+  UseFormReturn,
+  FieldValues
+} from 'react-hook-form'
+
+type ZodSchema = z.ZodType<FieldValues>
+type InferredZodSchema<T extends ZodSchema> = z.infer<T>
+type UseZodFormProps<T extends ZodSchema> = Omit<
+  UseFormProps<InferredZodSchema<T>>,
+  'resolver'
+> & {
+  schema: T
+}
+
+export default function useZodForm<T extends ZodSchema>({
+  schema,
+  ...formConfig
+}: UseZodFormProps<T>): UseFormReturn<InferredZodSchema<T>> {
+  const form = useForm<InferredZodSchema<T>>({
+    ...formConfig,
+    resolver: zodResolver(schema)
+  })
+
+  return form
+}
