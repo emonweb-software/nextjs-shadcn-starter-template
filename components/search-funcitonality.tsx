d81126ab6775fa4017e0e17d01d27e8e1ea8267f
tranvantu <tranvantu982001@gmail.com>
2024-11-04 16:14:16 +0700

    update

--- components/search-funcitonality.tsx
+++ components/search-funcitonality.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { Search } from "lucide-react";
+import { Search, X } from "lucide-react";
 import Link from "next/link";
 import React, { useState, useCallback } from "react";
+import { useRouter } from "next/navigation";
 const tempData = [
   "Viet Nam",
   "Hong Kong",
@@ -31,22 +32,24 @@ interface SearchBoxProps {
   setData: (data: string[]) => void;
   api: any;
 }
+
+const debounce = (func: Function, delay: number) => {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
 const SearchBox = () => {
+  const [isSearch, setIsSearch] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [isSearchContent, setSearchContent] = useState("");
-
-  const debounce = (func: Function, delay: number) => {
-    let timeoutId: NodeJS.Timeout;
-    return (...args: any[]) => {
-      if (timeoutId) clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  };
+  const router = useRouter();
 
   const callApi = (value: string) => {
-    alert(`call api with content: ${value}`);
+    console.log("call api with content", value);
   };
 
   const debouncedCallApi = useCallback(debounce(callApi, 1000), []);
@@ -57,23 +60,37 @@ const SearchBox = () => {
     setSearchContent(e.target.value);
     debouncedCallApi(e.target.value);
   };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      router.push(`/search?param=${isSearchContent}`);
+    }
+  };
   const filteredData = tempData.filter(item =>
     item.toLowerCase().includes(isSearchContent.toLowerCase())
   );
   const displayData = filteredData.length > 0 ? filteredData : ["No data"];
-
   return (
-    <div className="relative max-w-[490px] w-full">
-      <Search className="absolute top-1/2 left-[25px] -translate-y-1/2" />
+    <div className="relative w-fit">
+      <Search
+        onClick={() => {
+          setIsOpen(true);
+        }}
+        className={`size-[22px] text-gray-600 absolute ${isOpen ? "top-1/2 left-[14px] -translate-y-1/2" : "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "} duration-100 cursor-pointer`}
+      />
       <input
         type="text"
-        onFocus={() => setIsOpen(true)}
-        onBlur={() => setIsOpen(false)}
+        onFocus={() => setIsSearch(true)}
+        onBlur={() => {
+          setTimeout(() => {
+            setIsSearch(false);
+          }, 200);
+        }}
         onChange={handleChangeSearchContent}
-        className="p-[20px_32px_20px_70px] w-full h-[64px] outline-none rounded-xl bg-white shadow-[-6px_8px_16px_rgba(58,42,152,.1)] placeholder:text-black/50 border-[2px] border-[#F5F5F8] focus:border-[2px] focus:border-primary focus:shadow-none duration-200"
+        onKeyDown={handleKeyDown}
+        className={`${isOpen ? "flex-1 w-[180px] sm:w-[240px]  md:w-[330px] p-[12px_45px_12px_45px]" : "w-0 border-none outline-none p-[12px_10px_12px_45px]"} leading-[20px] text-[15px] duration-700 md:duration-500 outline-none border-[1px] border-gray-200 rounded-lg placeholder:text-black/80 placeholder:text-[14px] focus:outline focus:outline-gray-500 focus:outline-offset-0`}
         placeholder="Enter search content"
       />
-      {isOpen && (
+      {isSearch && (
         <div className="absolute top-[calc(100%+18px)] left-0 w-full ">
           <div className="px-[20px] bg-white rounded-lg h-auto max-h-[300px] sm:h-auto shadow-[-6px_8px_16px_rgba(58,42,152,.1)] overflow-y-scroll">
             <ul className="flex flex-col">
@@ -81,7 +98,7 @@ const SearchBox = () => {
                 return (
                   <li key={idx} className="">
                     <Link
-                      href="#"
+                      href={`/search?param=${item}`}
                       className="flex justify-between px-[14px] py-[12px] rounded-md hover:bg-gray-100">
                       <div className="flex items-center">
                         <span className="font-medium text-[18px]"> {item}</span>
@@ -93,6 +110,14 @@ const SearchBox = () => {
             </ul>
           </div>
         </div>
+      )}
+      {isOpen && (
+        <X
+          onClick={() => {
+            setIsOpen(false);
+          }}
+          className="z-[1] size-[20px] text-gray-500 hover:text-black absolute top-1/2 right-[12px] -translate-y-1/2 cursor-pointer"
+        />
       )}
     </div>
   );
