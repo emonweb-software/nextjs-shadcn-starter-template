674dc96b66bdd543f56937449dca248f7b3c5299
Vuong Do <xoaicc.work@gmail.com>
2024-11-05 20:34:12 +0700

    build: update home demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
-import { ModeToggle } from "@/components/toggle-theme";
-import SignInForm from "@/components/form/sign-in-form";
-import { LocaleSwitcherSelect } from "@/components/locale-switcher-select";
-import SearchBox from "@/components/search-funcitonality";
-
 export default function Home() {
   return (
     <div className="h-screen w-full flex justify-center items-center background">
-      <ModeToggle />
-      <LocaleSwitcherSelect />
-      <SignInForm />
-      <SearchBox />
+      <p>
+        Welcome to the Nextjs Shadcn Starter Template! This project serves as a
+        robust foundation for building modern web applications using Next.js and
+        TypeScript. With a focus on user experience, performance, and SEO, this
+        template incorporates essential features such as multi-language support,
+        form validation, and optimized image loading.
+      </p>
+      <p>
+        Whether you&#39;re a seasoned developer or just starting out, this
+        template provides a structured approach to developing scalable
+        applications. Dive in to explore the features and structure, and
+        customize it to fit your project&#39;s needs!
+      </p>
     </div>
   );
 }
